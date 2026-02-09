5899b8b5124c222f06bec23af7daecc34dc70404
ShallMelca <m0a22076f7@edu.teu.ac.jp>
2026-02-09 22:39:51 +0900

    進捗2

--- kubejs/server_scripts/src/common/changeSavetype.js
+++ kubejs/server_scripts/src/common/changeSavetype.js
@@ -9,11 +9,14 @@
     それぞれ一度のみ実行
 */
 
+let file = "[changeSavetype] "
+
 // server.persistantaData
 ServerEvents.loaded(event => {
     const { server } = event;
 
     if (server.persistentData.isMoved) return;
+    console.info(`${file}サーバーセーブデータを新形式に移行します`);
 
     const serverData = server.persistentData;
 
@@ -32,6 +35,8 @@ ServerEvents.loaded(event => {
         // データ移行.
         serverData.kings.score[title.key] = serverData[title.key + "_score"];
         serverData.kings.player[title.key] = serverData[title.key + "_player"];
+        console.info(`${file}${serverData[title.key + "_score"]} から ${serverData.kings.score[title.key]} へ移行しました`);
+        console.info(`${file}${serverData[title.key + "_player"]} から ${serverData.kings.player[title.key]} へ移行しました`);
 
         // 旧形式のデータを削除.
         serverData.remove(title.key + "_score");
@@ -52,6 +57,8 @@ PlayerEvents.loggedIn(event => {
     const playerData = player.persistentData;
     const serverData = server.persistentData;
 
+    console.info(`${file}プレイヤー ${player.name} (uuid:${player.uuid})さんに関するセーブデータを新形式に移行します`);
+
     playerData.isMoved = true;
 
     // 初期化
@@ -61,12 +68,14 @@ PlayerEvents.loggedIn(event => {
 
     // 移行
     playerData.kings.data[global.TITLES.HERO.key] = playerData.monsterkill;
+    console.info(`${file}${playerData.monsterkill} から ${playerData.kings.data[global.TITLES.HERO.key]} へ移行しました`);
 
+    // なぜかserver側に保存していたものをplayer管轄に移動
     const keysToMigrate = ["fallDeath", "lavaDeath"];
 
     keysToMigrate.forEach(keyName => {
         // 旧形式のキー（PlayerName_fallDeath）を作成.
-        let oldKey = player.username + "_" + keyName;
+        let oldKey = player.name + "_" + keyName;
 
         if (serverData.contains(oldKey)) {
             // サーバー側から値を取得.
@@ -75,13 +84,27 @@ PlayerEvents.loggedIn(event => {
             // プレイヤー側のデータに保存.
             playerData.kings.data[keyName] = value; // fallDeath,lavaDeath共にkeyそのままなので大丈夫なはず...
 
+            console.info(`${file}${serverData[oldKey]} から ${playerData.kings.data[keyName]} へ移行しました`);
+
             // サーバー側の古いデータを削除.
             serverData.remove(oldKey);
         }
     });
 
-    if (playerData.current_title_id == "PANDA") {
-        playerData.kings.titles += global.TITLES.PANDA.key;
+    // 種植え回数(農業王)
+    if (playerData[player.name + "_seed"]) {
+        playerData.kings.data[global.TITLES.FARMER.key] = playerData[player.name + "_seed"];
+        console.info(`${file}${serverData[player.name + "_seed"]} から ${playerData.kings.data[global.TITLES.FARMER.key]} へ移行しました`);
+        playerData.remove(`${player.name}_seed`);
     }
 
+    // 唯一解除済の挑戦型であるpandaに対する処理
+    if (playerData.current_title_id == "PANDA") {
+        // 解放済リストに入れる
+        let titleList = playerData.kings.titles;
+        titleList.push(global.TITLES.PANDA.key);
+        playerData.kings.titles = titleList;
+    }
+
+    playerData.kings.current_title_id == global.TITLES[playerData.current_title_id];
 })
