7531da79f4ba0a7cefe5d9c42aa06daeadc1593e
ShallMelca <m0a22076f7@edu.teu.ac.jp>
2026-02-09 16:46:04 +0900

    称号系コマンド 中身はまだない

---
+++ kubejs/server_scripts/src/title_cmds/applyTitle.js
@@ -0,0 +1,7 @@
+//priority 55
+
+/*
+    server_scripts/src/title_cmds/applyTitle.js
+    書いた人:シェイル
+    称号セットの手動実行 /titles apply ...
+*/
---
+++ kubejs/server_scripts/src/title_cmds/showTitle.js
@@ -0,0 +1,7 @@
+//priority 57
+
+/*
+    server_scripts/src/title_cmds/showTitle.js
+    書いた人:シェイル
+    所持称号表示コマンド /titles show
+*/
