75632bb6f3d49e23639373fb639e7adaa240f413
ShallMelca <m0a22076f7@edu.teu.ac.jp>
2026-02-10 18:03:53 +0900

    もっとスマートに、ユーザーネーム変更への耐性もつけながら、新形式へ移行

--- kubejs/server_scripts/src/common/checkHighScore.js
+++ kubejs/server_scripts/src/common/checkHighScore.js
@@ -8,14 +8,15 @@
 
 /**
  * 数値で記録更新をチェックし、必要なら称号を付け替える共通関数
- * @param {Internal.Server} server サーバーオブジェクト
  * @param {Internal.Player} player 操作対象のプレイヤー
  * @param {number} currentScore 今回のスコア
  * @param {any} titleData 付与する称号データ（titles.jsを参照）
  */
-global.checkHighScore = (server, player, currentScore, titleData) => {
-    // 永続データから今のトップ情報を取得.
-    let topScore = server.persistentData[titleData.key + "_score"] || 0;
+global.checkHighScore = (player, currentScore, titleData) => {
+    const server = player.server;
+
+    // 永続データから今のトップスコアを取得.
+    let topScore = server.persistentData.kings[titleData.key].score || 0;
 
     // 負荷軽減のために削除
     // console.info(`[${titleData.key}] Score: ${currentScore} / Record: ${topScore}`);
@@ -25,21 +26,21 @@ global.checkHighScore = (server, player, currentScore, titleData) => {
 
     if (currentScore <= topScore) return;
 
-    // 負荷軽減のために移動
-    let topPlayerName = server.persistentData[titleData.key + "_player"] || "";
+    // 負荷軽減のために移動　永続データから今のトッププレイヤーを取得
+    let topPlayerName = server.persistentData.kings[titleData.key].player || "";
 
     // 同一プレイヤーならスコア更新のみ.
-    if (player.username === topPlayerName) {
-        server.persistentData[titleData.key + "_score"] = currentScore;
+    if (player.username == topPlayerName || player.uuid == topPlayerName) { // uuidに変更するために一度両方記述
+        server.persistentData.kings[titleData.key].score = currentScore;
         return;
     }
 
     // 旧チャンピオンからの剥奪.
-    if (topPlayerName !== "") {
+    if (topPlayerName != "") {
         let oldChamp = server.getPlayer(topPlayerName);
         if (oldChamp) {
             global.ApplyTitle(oldChamp, global.TITLES.NONE);
-            // oldChamp.tell(Text.red(`${categoryName}世界一の座を奪われました！`));
+            oldChamp.tell(Text.red(`${categoryName}世界一の座を奪われました！`));
         }
     }
 
@@ -47,8 +48,8 @@ global.checkHighScore = (server, player, currentScore, titleData) => {
     global.CheckTitleRank(player, titleData);
 
     // 記録の保存.
-    server.persistentData[titleData.key + "_score"] = currentScore;
-    server.persistentData[titleData.key + "_player"] = player.username;
+    server.persistentData.kings[titleData.key].score = currentScore;
+    server.persistentData.kings[titleData.key].player = player.uuid;    // uuidに変更
 
     // 通知用変換.
     let prefix = titleData.display || "";
