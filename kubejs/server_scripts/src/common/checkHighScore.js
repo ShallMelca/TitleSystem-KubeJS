6b0491d26c6ffd3ceb15a74148ace97391b1a0a5
ShallMelca <m0a22076f7@edu.teu.ac.jp>
2026-02-10 20:50:16 +0900

    安全性を高め、なんか動かなかった部分をどうにかし、uuidをちゃんと保存できるようにStringに強制

--- kubejs/server_scripts/src/common/checkHighScore.js
+++ kubejs/server_scripts/src/common/checkHighScore.js
@@ -14,9 +14,11 @@
  */
 global.checkHighScore = (player, currentScore, titleData) => {
     const server = player.server;
+    const serverData = server.persistentData.kings || (server.persistentData.kings = {});
+    const currentData = serverData[titleData.key] || (serverData[titleData.key] = {});
 
     // 永続データから今のトップスコアを取得.
-    let topScore = server.persistentData.kings[titleData.key].score || 0;
+    let topScore = currentData.score || 0;
 
     // 負荷軽減のために削除
     // console.info(`[${titleData.key}] Score: ${currentScore} / Record: ${topScore}`);
@@ -27,20 +29,22 @@ global.checkHighScore = (player, currentScore, titleData) => {
     if (currentScore <= topScore) return;
 
     // 負荷軽減のために移動　永続データから今のトッププレイヤーを取得
-    let topPlayerName = server.persistentData.kings[titleData.key].player || "";
+    let topPlayer = currentData.player || "";
+
+    // console.info(`[chechHighScore] topPlayer:[${topPlayer}], nowPL:[${player.uuid}]`);
 
     // 同一プレイヤーならスコア更新のみ.
-    if (player.username == topPlayerName || player.uuid == topPlayerName) { // uuidに変更するために一度両方記述
-        server.persistentData.kings[titleData.key].score = currentScore;
+    if (player.username === topPlayer || player.uuid === topPlayer) { // uuidに変更するために一度両方記述
+        currentData.score = currentScore;
         return;
     }
 
     // 旧チャンピオンからの剥奪.
-    if (topPlayerName != "") {
-        let oldChamp = server.getPlayer(topPlayerName);
+    if (topPlayer != "") {
+        let oldChamp = server.getPlayer(topPlayer);
         if (oldChamp) {
             global.ApplyTitle(oldChamp, global.TITLES.NONE);
-            oldChamp.tell(Text.red(`${categoryName}世界一の座を奪われました！`));
+            oldChamp.tell(Text.red(`${titleData.display}世界一の座を奪われました！`));
         }
     }
 
@@ -49,12 +53,11 @@ global.checkHighScore = (player, currentScore, titleData) => {
 
     // 記録の保存.
     server.persistentData.kings[titleData.key].score = currentScore;
-    server.persistentData.kings[titleData.key].player = player.uuid;    // uuidに変更
+    server.persistentData.kings[titleData.key].player = String(player.uuid);    // uuidに変更
 
     // 通知用変換.
     let prefix = titleData.display || "";
 
     // 全員通知.
     server.tell(Text.gold(`[速報] ${player.username} がスコア ${currentScore} で新「${prefix}」になりました！`));
-
 };
